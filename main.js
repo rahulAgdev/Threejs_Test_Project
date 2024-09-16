b79678f407273a7d270059f3a78a4348c87369db
Rahul <zentrades@ZenTradess-MacBook-Pro.local>
2024-09-16 20:40:32 +0530

    Adding light to animation

--- main.js
+++ main.js
@@ -1,30 +1,38 @@
 import * as THREE from 'three';
-// adding scene (sort of like canvas)
+
+// Create the scene
 const scene = new THREE.Scene();
-// adding camera (viewpoint source)
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-// adding renderer (to render the threejs model)
-const renderer = new THREE.WebGLRenderer();
-// setting renderer size
-renderer.setSize(window.innerWidth, window.innerHeight);
-// appending to document
-document.body.appendChild(renderer.domElement);
-// adding geometry
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-// adding material
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-// creating mesh cube with geometry and material
-const cube = new THREE.Mesh(geometry, material);
-// adding cube to scene
-scene.add(cube);
-// distancing the camera away from the origin
+scene.background = new THREE.Color('#F0F0F0');
+
+// Add the camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
 
-// creating render/animation loop
-function animate() {
-    // giving rotation to the cube
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+// create and add object
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshLambertMaterial({color: '#468585', emissive: '#468585'})
+const cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube);
+
+// add lighting
+const light = new THREE.DirectionalLight(0x9CDBA6, 10);
+light.position.set(1,1,1);
+scene.add(light);
+
+// set up the renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// animate the scene
+function animate(){
+    requestAnimationFrame(animate);
+    cube.rotation.x +=0.01;
+    cube.rotation.y +=0.01;
     renderer.render(scene, camera);
+
+    
 }
-renderer.setAnimationLoop(animate);
+
+animate();
